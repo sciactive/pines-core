663ae0ee9845cf6edc5c8272fa549a584962212f
Hunter Perrin <hperrin@gmail.com>
2009-10-09 19:23:05 +0000

    Removed old unfinished table class. It is unnecessary now because of pgrid.

--- display.php
+++ display.php
@@ -19,37 +19,6 @@ $config->current_template = ( !empty($_REQUEST['template']) && $config->allow_te
 	$_REQUEST['template'] : $config->default_template;
 require_once('templates/'.$config->current_template.'/configure.php');
 
-/*
-class table {
-	public $name, $id, $class, $style = '';
-	public $table_array = array();
-
-	/*
-	 * $info can contain:
-	 * type: th, td, or tf
-	 * index: an integer
-	 * name: a string
-	 * id: a string
-	 * class: a string
-	 * style: a string
-	 *
-	function add_row($cells = array(), $info = array('type' => 'td')) {
-		if ( isset($info['index']) ) {
-			//TODO: finish this table class
-		}
-	}
-
-	function render() {
-		$return = '<table';
-		$return .= empty($this->name) ? '' : ' name="'.$this->name.'"';
-		$return .= empty($this->id) ? '' : ' id="'.$this->id.'"';
-		$return .= empty($this->class) ? '' : ' class="'.$this->class.'"';
-		$return .= empty($this->style) ? '' : ' style="'.$this->style.'"';
-		$return .= ">\n";
-	}
-}
- */
-
 /**
  * The page controller's variable. One of the few objects not under $config.
  * @global page $page
