f17c1cdbac731711ebf6470b12257eb5627545b0
Hunter Perrin <hperrin@gmail.com>
2009-10-27 20:16:07 +0000

    Added indexes to MySQL tables.

--- pines.sql
+++ pines.sql
@@ -5,12 +5,14 @@ CREATE TABLE IF NOT EXISTS `pin_com_entity_data` (
   `guid` bigint(20) NOT NULL,
   `name` text NOT NULL,
   `value` text NOT NULL,
-  PRIMARY KEY  (`id`)
+  PRIMARY KEY  (`id`),
+  KEY `id_guid` (`guid`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 CREATE TABLE IF NOT EXISTS `pin_com_entity_entities` (
   `guid` bigint(20) NOT NULL auto_increment,
   `parent` bigint(20) default NULL,
   `tags` text,
-  PRIMARY KEY  (`guid`)
+  PRIMARY KEY  (`guid`),
+  KEY `id_parent` (`parent`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
