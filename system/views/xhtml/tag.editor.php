97df1b797bca4addda129384312679af0bea9f89
Hunter Perrin <hperrin@gmail.com>
2009-10-29 21:05:46 +0000

    Added Tag Editor jQuery plugin. More work on new product form.

---
+++ system/views/xhtml/tag.editor.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * A view to load the Tag Editor jQuery plugin.
+ *
+ * Attach this view if your module uses the Tag Editor.
+ *
+ * @package Pines
+ * @license http://www.fsf.org/licensing/licenses/agpl-3.0.html
+ * @author Hunter Perrin <hunter@sciactive.com>
+ * @copyright Hunter Perrin
+ * @link http://sciactive.com/
+ */
+defined('P_RUN') or die('Direct access prohibited');
+
+// Check to see if Tag Editor is already included.
+if (!$GLOBALS['js_tageditor_included']) {
+    $page->head("<link href=\"{$config->rela_location}system/css/jquery.tag.editor.css\" media=\"all\" rel=\"stylesheet\" type=\"text/css\" />");
+    $page->head("<script type=\"text/javascript\" src=\"{$config->rela_location}system/js/jquery.tag.editor.js\"></script>");
+    $GLOBALS['js_tageditor_included'] = true;
+}
+
+return;
+?>
