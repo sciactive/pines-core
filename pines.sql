3d5b010d5ed4c1b5123b263a0a359d8fdbe359c7
Hunter Perrin <hperrin@gmail.com>
2009-11-11 02:12:48 +0000

    Fixed pines.sql.

--- pines.sql
+++ pines.sql
@@ -1,8 +1,8 @@
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
-CREATE USER 'pines'@'localhost' IDENTIFIED BY PASSWORD 'password';
+CREATE USER 'pines'@'localhost' IDENTIFIED BY 'password';
 
-GRANT USAGE ON *.* TO 'pines'@'localhost' IDENTIFIED BY PASSWORD 'password'
+GRANT USAGE ON *.* TO 'pines'@'localhost'
 WITH
     MAX_QUERIES_PER_HOUR 0
     MAX_CONNECTIONS_PER_HOUR 0
@@ -10,6 +10,8 @@ WITH
     MAX_USER_CONNECTIONS 0;
 
 CREATE DATABASE IF NOT EXISTS `pines`;
+
+USE `pines`;
 
 GRANT ALL PRIVILEGES ON `pines`.* TO 'pines'@'localhost';
 
