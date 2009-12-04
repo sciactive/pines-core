1f45ac89626f923ddcede060f6b0c04468b04492
Hunter Perrin <hperrin@gmail.com>
2009-12-04 20:57:54 +0000

    Work on jQuery plugins.

---
+++ system/views/xhtml/ptags.default.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * A view to load the Pines Tags jQuery plugin.
+ *
+ * Attach this view if your module uses Pines Tags. It uses the default Pines
+ * Tags theme.
+ *
+ * @package Pines
+ * @license http://www.fsf.org/licensing/licenses/agpl-3.0.html
+ * @author Hunter Perrin <hunter@sciactive.com>
+ * @copyright Hunter Perrin
+ * @link http://sciactive.com/
+ */
+defined('P_RUN') or die('Direct access prohibited');
+
+// Check to see if ptags is already included.
+if (!$GLOBALS['js_ptags_included']) {
+	echo ("<link href=\"{$config->rela_location}system/css/jquery.ptags.default.css\" media=\"all\" rel=\"stylesheet\" type=\"text/css\" />\n");
+	echo ("<script type=\"text/javascript\" src=\"{$config->rela_location}system/js/jquery.ptags.js\"></script>\n");
+	$GLOBALS['js_ptags_included'] = true;
+}
+
+?>
