4b13b32cd86de590556509e1063cc2bcfcdcbb96
Hunter Perrin <hperrin@gmail.com>
2010-01-28 02:31:15 +0000

    New option for entity retrieval, references. Get entities which reference an entity. Entity manager improvements as well. Entity manager is now several times faster.

--- pines.sql
+++ pines.sql
@@ -1,19 +1,19 @@
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
-CREATE USER 'pines'@'localhost' IDENTIFIED BY 'password';
+/* CREATE USER 'pines'@'localhost' IDENTIFIED BY 'password';
 
 GRANT USAGE ON *.* TO 'pines'@'localhost'
 WITH
 	MAX_QUERIES_PER_HOUR 0
 	MAX_CONNECTIONS_PER_HOUR 0
 	MAX_UPDATES_PER_HOUR 0
-	MAX_USER_CONNECTIONS 0;
+	MAX_USER_CONNECTIONS 0; */
 
 CREATE DATABASE IF NOT EXISTS `pines`;
 
 USE `pines`;
 
-GRANT ALL PRIVILEGES ON `pines`.* TO 'pines'@'localhost';
+/* GRANT ALL PRIVILEGES ON `pines`.* TO 'pines'@'localhost'; */
 
 CREATE TABLE IF NOT EXISTS `pin_com_entity_data` (
   `id` bigint(20) NOT NULL auto_increment,
