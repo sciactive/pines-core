3944ac744e019f9d7c45896f7b8aca3733635956
Hunter Perrin <hperrin@gmail.com>
2010-04-06 16:02:31 -0700

    New log manager interface. Reorganized system init scripts.

--- pines/system/init/i20interfaces.php
+++ pines/system/init/i20interfaces.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Define some basic functions and service interfaces.
+ * Define system service interfaces.
  *
  * @package Pines
  * @license http://www.gnu.org/licenses/agpl-3.0.html
@@ -10,70 +10,7 @@
  */
 defined('P_RUN') or die('Direct access prohibited');
 
-/**
- * Scan a directory and filter the results.
- *
- * Scan a directory and filter any dot files/dirs and "index.html" out of the
- * result.
- *
- * @param string $directory The directory that will be scanned.
- * @param int $sorting_order By default, the sorted order is alphabetical in ascending order. If the optional sorting_order is set to non-zero, then the sort order is alphabetical in descending order.
- * @param resource $context An optional context.
- * @param bool $hide_dot_files Whether to hide filenames beginning with a dot.
- * @return array|false The array of filenames on success, false on failure.
- */
-function pines_scandir($directory, $sorting_order = 0, $context = null, $hide_dot_files = true) {
-	if (isset($context)) {
-		if (!($return = scandir($directory, $sorting_order, $context))) return false;
-	} else {
-		if (!($return = scandir($directory, $sorting_order))) return false;
-	}
-	foreach ($return as $cur_key => $cur_name) {
-		if ( (stripos($cur_name, '.') === 0 && $hide_dot_files) || (in_array($cur_name, array('index.html', '.', '..', '.svn'))) )
-			unset($return[$cur_key]);
-	}
-	return array_values($return);
-}
-
-/**
- * Strip slashes from an array recursively.
- *
- * Only processes strings.
- *
- * @param array &$array The array to process.
- * @return bool True on success, false on failure.
- */
-function pines_stripslashes_array_recursive(&$array) {
-	if (!is_array($array)) return false;
-	foreach ($array as &$cur_item) {
-		if (is_array($cur_item)) {
-			pines_stripslashes_array_recursive($cur_item);
-		} elseif (is_string($cur_item)) {
-			$cur_item = stripslashes($cur_item);
-		}
-	}
-	return true;
-}
-
-/**
- * Sort by only the file's name.
- *
- * If the file's names are equal, then the entire string is compared using
- * strcmp(), otherwise, only the filename is compared.
- *
- * @param string $a The first file.
- * @param string $b The second file.
- * @return int Compare result.
- */
-function pines_sort_by_filename($a, $b) {
-	$str1 = strrchr($a, '/');
-	$str2 = strrchr($b, '/');
-	if ($str1 == $str2) {
-		return strcmp($a, $b);
-	} else {
-		return strcmp($str1, $str2);
-	}
-}
+if (P_SCRIPT_TIMING) pines_print_time('Define Service Interfaces');
 
 /**
  * Objects which hold data from some type of storage.
@@ -267,7 +204,7 @@ interface group_interface extends able_object_interface {
 	public function get_logo($rela_location = false);
 	/**
 	 * Print a form to edit the group.
-	 * 
+	 *
 	 * @return module The form's module.
 	 */
 	public function print_form();
@@ -280,7 +217,7 @@ interface group_interface extends able_object_interface {
 interface template_interface {
 	/**
 	 * Format a menu.
-	 * 
+	 *
 	 * @param array $menu The menu.
 	 * @return string The menu's resulting code.
 	 */
@@ -336,15 +273,15 @@ interface configurator_interface {
 
 /**
  * A configurable component.
- * 
+ *
  * Must have the following public variables:
- * 
+ *
  * - $defaults - The configuration defaults.
  * - $config - The current configuration.
  * - $config_keys - The current configuration in an array with key => values.
  * - $info - The info object of the component.
  * - $name - The component.
- * 
+ *
  * @package Pines
  */
 interface configurator_component_interface {
@@ -389,5 +326,22 @@ interface configurator_component_interface {
 	 */
 	public function save_config();
 }
+
+/**
+ * Logs activity within the framework.
+ * @package Pines
+ */
+interface log_manager_interface {
+	/**
+	 * Log an entry to the Pines log.
+	 *
+	 * @param string $message The message to be logged.
+	 * @param string $level The level of the message. (debug, info, notice, warning, error, or fatal)
+	 * @return bool True on success, false on error.
+	 */
+	public function log($message, $level = 'info');
+}
+
+if (P_SCRIPT_TIMING) pines_print_time('Define Service Interfaces');
 
 ?>
