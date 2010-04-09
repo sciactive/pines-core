81f50c91e09b01189297a9d8ce18c489a87cb748
Hunter Perrin <hperrin@gmail.com>
2010-04-09 13:01:55 -0700

    Some JavaScript fixes for IE.

--- pines/system/js/common.js
+++ pines/system/js/common.js
@@ -2,28 +2,62 @@
  * Pines Common JavaScript
  */
 // Make sure arrays have an indexOf method.
-if (!Array.prototype.indexOf)
-{
-  Array.prototype.indexOf = function(elt /*, from*/)
-  {
-    var len = this.length >>> 0;
-
-    var from = Number(arguments[1]) || 0;
-    from = (from < 0)
-         ? Math.ceil(from)
-         : Math.floor(from);
-    if (from < 0)
-      from += len;
-
-    for (; from < len; from++)
-    {
-      if (from in this &&
-          this[from] === elt)
-        return from;
-    }
-    return -1;
-  };
+if (!Array.prototype.indexOf) {
+	Array.prototype.indexOf = function(elt /*, from*/) {
+		var len = this.length >>> 0;
+		var from = Number(arguments[1]) || 0;
+		from = (from < 0) ? Math.ceil(from) : Math.floor(from);
+		if (from < 0)
+			from += len;
+		for (; from < len; from++) {
+			if (from in this &&
+				this[from] === elt)
+			return from;
+		}
+		return -1;
+	};
 }
+/*
+// Create addEventListener and attachEvent if they don't exist.
+function createIEaddEventListeners() {
+    if (document.addEventListener || !document.attachEvent)
+        return;
+    function ieAddEventListener(eventName, handler, capture) {
+        if (this.attachEvent)
+            this.attachEvent('on' + eventName, handler);
+    }
+    function attachToAll() {
+        var i, l = document.all.length;
+        for (i = 0; i < l; i++)
+            if (document.all[i].attachEvent)
+                document.all[i].addEventListener = ieAddEventListener;
+    }
+    var originalCreateElement = document.createElement;
+    document.createElement = function(tagName) {
+        var element = originalCreateElement(tagName);
+        if (element.attachEvent)
+            element.addEventListener = ieAddEventListener;
+        return element;
+    }
+    window.addEventListener = ieAddEventListener;
+    document.addEventListener = ieAddEventListener;
+    var body = document.body;
+    if (body) {
+        if (body.onload) {
+            var originalBodyOnload = body.onload;
+            body.onload = function() {
+                attachToAll();
+                originalBodyOnload();
+            };
+        }
+        else
+            body.onload = attachToAll;
+    }
+    else
+        window.addEventListener('load', attachToAll);
+}
+createIEaddEventListeners();
+*/
 
 var pines = {
 	full_location: "",
