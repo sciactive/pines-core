2a5c8ebb7aeb1bec986f2975535063eb515ce6fb
Hunter Perrin <hperrin@gmail.com>
2010-04-13 10:03:58 -0700

    Updated install and readme files. Incremented version number to 0.60.

--- pines/system/info.php
+++ pines/system/info.php
@@ -13,10 +13,10 @@ defined('P_RUN') or die('Direct access prohibited');
 return array(
 	'name' => 'Pines',
 	'author' => 'SciActive',
-	'version' => '0.55.0alpha',
+	'version' => '0.60.0alpha',
 	'license' => 'http://www.gnu.org/licenses/agpl-3.0.html',
 	'short_description' => 'Pines PHP framework core system',
-	'description' => 'The base system of the Pines PHP application framework.',
+	'description' => 'The core system of the Pines PHP application framework.',
 );
 
 ?>
