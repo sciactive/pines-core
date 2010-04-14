4d1a926db398e88254586c78c405f05520cc76c9
Hunter Perrin <hperrin@gmail.com>
2010-04-14 15:00:47 -0700

    Work on per user/group configuration.

--- pines/system/defaults.php
+++ pines/system/defaults.php
@@ -28,6 +28,7 @@ return array(
 		'cname' => 'Upload Directory',
 		'description' => 'The directory to store uploaded files. This should be the real, relative path and the relative URL. End this path with a slash!',
 		'value' => 'media/',
+		'peruser' => true,
 	),
 	array(
 		'name' => 'offline_mode',
@@ -46,30 +47,35 @@ return array(
 		'cname' => 'Page Title',
 		'description' => 'The default title at the top of each page.',
 		'value' => 'Pines',
+		'peruser' => true,
 	),
 	array(
 		'name' => 'option_copyright_notice',
 		'cname' => 'Copyright Notice',
 		'description' => 'The copyright notice at the bottom of each page.',
 		'value' => '&copy; 2010 SciActive.com. All Rights Reserved. Powered by <a href="http://pines.sourceforge.net/" onclick="window.open(this.href); return false;">Pines</a>.',
+		'peruser' => true,
 	),
 	array(
 		'name' => 'default_template',
 		'cname' => 'Default Template',
 		'description' => 'The default template.',
 		'value' => 'tpl_pines',
+		'peruser' => true,
 	),
 	array(
 		'name' => 'allow_template_override',
 		'cname' => 'Template Override',
 		'description' => 'Allow the template to be overriden by adding ?template=whatever',
 		'value' => true,
+		'peruser' => true,
 	),
 	array(
 		'name' => 'url_rewriting',
 		'cname' => 'URL Rewriting',
 		'description' => 'Use url rewriting engine.',
 		'value' => false,
+		'peruser' => true,
 	),
 	array(
 		'name' => 'use_htaccess',
@@ -82,18 +88,21 @@ return array(
 		'cname' => 'Default Component',
 		'description' => 'This component should have a "default" action. That action will be called when the user first accesses Pines. If an action is specified, but no component, this one will be used.',
 		'value' => 'com_user',
+		'peruser' => true,
 	),
 	array(
 		'name' => 'timezone',
 		'cname' => 'System Timezone',
 		'description' => 'The timezone the system should use as its default. User\'s timezones will default to this.',
 		'value' => date_default_timezone_get(),
+		'peruser' => true,
 	),
 	array(
 		'name' => 'debug_mode',
 		'cname' => 'Debug Mode',
 		'description' => 'Only use debug mode during testing. When debug mode is enabled, components will use non-minified versions of their JavaScripts (if available), in order to make debugging easier.',
 		'value' => false,
+		'peruser' => true,
 	),
 );
 
