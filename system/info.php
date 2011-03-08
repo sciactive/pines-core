8e183ba2348d85ac23aac6c10991c4f3ce10fa68
Hunter Perrin <hperrin@gmail.com>
2011-03-07 21:17:23 -0800

    Incremented version number to 0.94.0beta.

--- system/info.php
+++ system/info.php
@@ -13,7 +13,7 @@ defined('P_RUN') or die('Direct access prohibited');
 return array(
 	'name' => 'Pines',
 	'author' => 'SciActive',
-	'version' => '0.92.0beta',
+	'version' => '0.94.0beta',
 	'license' => 'http://www.gnu.org/licenses/agpl-3.0.html',
 	'website' => 'http://www.sciactive.com',
 	'short_description' => 'Pines PHP framework core system',
