c7b717e1e6b86fd17d24b9717c633a8cf20b16d5
Hunter Perrin <hperrin@gmail.com>
2011-06-03 16:33:32 -0700

    Preparations for switching from XHTML to HTML5.

--- system/template_error.php
+++ system/template_error.php
@@ -9,12 +9,11 @@
  * @link http://sciactive.com/
  */
 defined('P_RUN') or die('Direct access prohibited');
-?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-
+header('Content-Type: text/html');
+?><!DOCTYPE html>
+<html>
 <head>
+	<meta charset="utf-8" />
 	<title><?php echo htmlspecialchars($this->config->page_title); ?></title>
 	<style type="text/css" media="all">
 		/* <![CDATA[ */
