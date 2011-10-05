6f2edc252228ffbc453d48815efb1df6788f768c
Hunter Perrin <hperrin@gmail.com>
2011-10-05 14:31:02 -0700

    Added correct content types.

--- system/actions/date_format.php
+++ system/actions/date_format.php
@@ -14,6 +14,7 @@ if (!gatekeeper())
 	punt_user();
 
 $pines->page->override = true;
+header('Content-Type: text/plain');
 $pines->page->override_doc(format_date(!is_numeric($_REQUEST['timestamp']) ? time() : (int) $_REQUEST['timestamp'], empty($_REQUEST['type']) ? 'full_sort' : $_REQUEST['type'], empty($_REQUEST['format']) ? '' : $_REQUEST['format'],  empty($_REQUEST['timezone']) ? null : new DateTimeZone($_REQUEST['timezone'])));
 
 ?>
