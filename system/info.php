a7ea5096cc8f5a9d8adf0bf601ffb88df30ea41f
Hunter Perrin <hperrin@gmail.com>
2012-11-02 12:34:56 -0700

    Updated the PHP version requirement to be accurate.

--- system/info.php
+++ system/info.php
@@ -14,13 +14,13 @@ defined('P_RUN') or die('Direct access prohibited');
 return array(
 	'name' => 'Pines',
 	'author' => 'SciActive',
-	'version' => '2.0.0',
+	'version' => '2.0.1dev',
 	'license' => 'http://www.gnu.org/licenses/agpl-3.0.html',
 	'website' => 'http://pinesframework.org/',
 	'short_description' => 'Pines framework core system.',
 	'description' => 'The core system of the Pines PHP framework.',
 	'depend' => array(
-		'php' => '>=5.2.10'
+		'php' => '>=5.3.0'
 	),
 	'abilities' => array(
 		array('all', 'All Abilities', 'Let user do anything, regardless of whether they have the ability.')
